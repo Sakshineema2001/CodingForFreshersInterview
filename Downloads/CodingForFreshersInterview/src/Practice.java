96ce19e1be77f7d2f21b13c3b3d2deae58d09034
sakshineema <sakshineema2001@gmail.com>
2023-05-23 17:02:37 +0530

    reverse

--- Downloads/CodingForFreshersInterview/src/Practice.java
+++ Downloads/CodingForFreshersInterview/src/Practice.java
@@ -10,9 +10,12 @@ public class Practice
 		System.out.println(fibonacci(12));
 		System.out.println(swap("123455",2,3));
 		permute(str,0,str.length()-1);
+		reverse("sakshi");
+		System.out.println(reverseByLoop("sakshi"));
 	}
 
 
+	// check if string is isogram or not  means not having any duplicate character
 	public static Boolean isIsogram(String s){
 		boolean isogram = true;
 		char[] ch = s.toCharArray();
@@ -27,6 +30,7 @@ public class Practice
 		return isogram;
 	}
 
+	// check is it is fibonacci
 	public static int fibonacci(int n){
 		if(n <= 1){
 			return n;
@@ -36,6 +40,7 @@ public class Practice
 		}
 	}
 
+	//check string is permutate means each possible way to arrange ex -puzzle
 	public static void  permute(String str,int left , int right){
 		if(left == right){
 			System.out.println(str);
@@ -47,11 +52,28 @@ public class Practice
 			}
 		}
 	}
+
+	//swap on specific position of numbers
 	public static String swap(String str, int i, int j){
 		char[] ch = str.toCharArray();
 		char temp = ch[i];
 		ch[i] = ch[j];
 		ch[j] = temp;
 		return String.valueOf(ch);
+	}
+
+	//reverse using function of string buffer and builder
+	public static void reverse(String str){
+		StringBuffer stringBuffer = new StringBuffer(str);
+		System.out.println(stringBuffer.reverse());
+	}
+
+	//reverse String by loop
+	public static String reverseByLoop(String str){
+       StringBuilder stringBuilder = new StringBuilder();
+		for(int i=str.length()-1;i>=0;i--){
+          stringBuilder.append(str.charAt(i));
+		}
+		return stringBuilder.toString();
 	}
 }
